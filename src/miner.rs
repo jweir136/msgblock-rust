ce8aae35db4b61093fc5963f8ecb14876839dee1
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-23 14:17:51 -0500

    add Miner and Miner::new()

--- src/miner.rs
+++ src/miner.rs
@@ -1,2 +1,25 @@
 use crate::types::{ Hash, Seal };
-use crate::block;
+use crate::block::{ MsgBlock };
+use std::thread;
+use std::net::{ TcpListener, TcpStream };
+
+pub struct Miner {
+    server: TcpListener,
+    addr: String,
+    blocks: Vec<MsgBlock>,
+    peers: Vec<String>
+}
+
+impl Miner {
+    pub fn new(ip: String) -> Self {
+        Miner {
+            server: TcpListener::bind(&ip).expect("Error: Cannot start server"),
+            addr: ip,
+            blocks: Vec::<MsgBlock>::new(),
+            peers: vec![
+                "localhost:8000".to_string(), "localhost:8000".to_string(),
+                "localhost:8000".to_string(), "localhost:8000".to_string()
+            ]
+        }
+    }
+}
